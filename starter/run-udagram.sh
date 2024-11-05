c11a548a5f595c3506d8ea830987410c28a54227
root <root@vmi2217633.contaboserver.net>
2024-11-06 06:40:48 +0700

    init udagram

---
+++ starter/run-udagram.sh
@@ -0,0 +1,12 @@
+# aws cloudformation validate-template --profile udacity --template-body file://udagram.yml;
+
+
+aws cloudformation create-stack \
+    --profile udacity \
+    --template-body file://udagram.yml \
+    --stack-name UdagramStack \
+    --region us-east-1 \
+    --parameters file://udagram-parameters.json \
+    --capabilities "CAPABILITY_NAMED_IAM" ;
+
+# aws cloudformation delete-stack --profile udacity --stack-name TestStack;
