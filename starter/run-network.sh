766da916526f72ea0d1bf01af46b2e7381ba6309
root <root@vmi2217633.contaboserver.net>
2024-11-06 05:37:37 +0700

    done network

--- starter/run-network.sh
+++ starter/run-network.sh
@@ -1,4 +1,11 @@
-# aws cloudformation create-stack --profile udacity --template-body file://network.yml --stack-name TestStack \
-#  --region us-east-1 --parameters file://network-parameters.json --capabilities "CAPABILITY_NAMED_IAM" ;
+aws cloudformation validate-template --profile udacity --template-body file://network.yml;
 
-aws cloudformation delete-stack --profile udacity --stack-name TestStack;
+aws cloudformation create-stack \
+    --profile udacity \
+    --template-body file://network.yml \
+    --stack-name TestStack \
+    --region us-east-1 \
+    --parameters file://network-parameters.json \
+    --capabilities "CAPABILITY_NAMED_IAM" ;
+
+# aws cloudformation delete-stack --profile udacity --stack-name TestStack;
