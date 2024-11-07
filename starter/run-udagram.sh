03858e56e81161f52f51db9b475a70e94826e4ea
root <root@vmi2217633.contaboserver.net>
2024-11-08 01:11:14 +0700

    fix

--- starter/run-udagram.sh
+++ starter/run-udagram.sh
@@ -1,6 +1,6 @@
 # aws cloudformation validate-template --profile udacity --template-body file://udagram.yml;
 
-aws cloudformation update-stack \
+aws cloudformation create-stack \
     --template-body file://udagram.yml \
     --stack-name UdagramStack \
     --region us-east-1 \
